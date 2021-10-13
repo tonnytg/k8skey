182efb928bce2c0b8399961219be74dfc8009806
Antonio Thomacelli <tonnytg@gmail.com>
2021-10-12 22:34:47 -0300

    feat: add new set shell to ZSH

--- pkg/cmd/cmd.go
+++ pkg/cmd/cmd.go
@@ -6,17 +6,25 @@ import (
 	"k8skey/pkg/clusters"
 	"k8skey/pkg/interactive"
 	"k8skey/pkg/projects"
+	"log"
 	"os"
 )
 
 func Flags() {
 	db := flag.String("database", "nil", "--database update")
 	con := flag.Bool("select", false, "--select true")
+	prompt := flag.String("prompt", "off", "--prompt on")
 	project := flag.String("project", "", "--project PROJECT_ID")
 	cluster := flag.String("cluster", "", "--cluster CLUSTER")
 	region := flag.String("region", "", "--region REGION")
 
 	flag.Parse()
+
+	if *prompt == "on" {
+		SetShell()
+		os.Exit(0)
+	}
+
 	if *db == "update" {
 		fmt.Println("Updating database of projects and clusters")
 		projects.List()
@@ -39,4 +47,23 @@ func Flags() {
 	} else {
 		os.Exit(1)
 	}
+}
+
+func SetShell() {
+
+	dir, err := os.UserHomeDir()
+	err = os.Mkdir(dir+"/.k8skey", 0755)
+
+	file := dir + "/.k8skey/kube_ps1.zsh"
+
+	f, err := os.Create(file)
+	defer f.Close()
+	if err != nil {
+		log.Println(err)
+	}
+
+	f.WriteString("PROMPT='$(kube_ps1)'$PROMPT\n")
+
+	fmt.Println("source  ~/.k8skey/kube_ps1.zsh")
+	os.Exit(0)
 }
